fc011eb3504dc8ac65fe4110d63e4f7fb9074da9
Marcin Polak <mapoart@gmail.com>
2020-08-31 23:33:03 +0200

    Fix default template / default.tcl.js - config for installation of the template

--- templates/default.tcl.js
+++ templates/default.tcl.js
@@ -1,12 +1,12 @@
 // additional info for templates like copy extra libraries.
 // in this case library needs JSON
 const config = {
-  files: ["3rdPartyLibraries/jimhttp/json.tcl"],
+  files: ["3rdPartyLibraries/tcl2json/tcl2json.tcl"],
   commands: [""],
   repos: ["https://rosettacode.org/wiki/JSON#Tcl"],
   descriptions: [
-    "file: tcl2json.tcl based on rosettacode.org, GNU Free Documentation License 1.2"
-  ]
+    "file: tcl2json.tcl based on rosettacode.org, GNU Free Documentation License 1.2",
+  ],
 };
 
 module.exports = config;
