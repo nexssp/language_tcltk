d9f18a4a0a42285705fecd488b1151c938d25fc3
Marcin Polak <mapoart@gmail.com>
2021-04-28 00:49:32 +0200

    The same applies for hello world (wget not found)

--- templates/helloWorld.tcl.js
+++ templates/helloWorld.tcl.js
@@ -1,12 +1,19 @@
 // additional info for templates like copy extra libraries.
 // in this case library needs JSON
+let install = "";
+if (process.platform !== "win32") {
+  switch (process.distro) {
+    case process.distros.CENTOS:
+      break;
+    default:
+      const { tclLibCustomInstall } = require("./../tcltk_installs");
+      install = process.replacePMByDistro(tclLibCustomInstall());
+  }
+}
+
 const config = {
   files: ["3rdPartyLibraries/tcl2json/tcl2json.tcl"],
-  commands: [
-    process.platform !== "win32"
-      ? process.replacePMByDistro("apt-get install tcllib")
-      : "",
-  ],
+  commands: [install],
   repos: ["https://rosettacode.org/wiki/JSON#Tcl"],
   descriptions: [
     "file: tcl2json.tcl based on rosettacode.org, GNU Free Documentation License 1.2",
